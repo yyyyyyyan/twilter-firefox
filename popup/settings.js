13aff3774db2fbdf5bbd45ad466a1a678818745c
Yan <yanorestes@hotmail.com>
2018-12-05 17:09:40 -0200

    Settings done and first steps on twilter.js

--- popup/settings.js
+++ popup/settings.js
@@ -1,22 +1,50 @@
+function createLi(toFilter, filterTo) {
+	let li = document.createElement('li');
+	let liText = document.createTextNode(toFilter + " - " + filterTo);
+	li.appendChild(liText);
+	li.setAttribute('id', toFilter);
+
+	let removeBtn = document.createElement('a');
+	removeBtn.setAttribute('href', '#');
+	let removeIcon = document.createElement('i');
+	removeIcon.setAttribute('class', 'material-icons');
+	let removeText = document.createTextNode('delete');
+	removeIcon.appendChild(removeText)
+	removeBtn.appendChild(removeIcon);
+	removeBtn.setAttribute('class', 'removeBtn');
+
+	removeBtn.onclick = function() {
+		let filterToRemove = this.parentElement.id;
+		let filterType = this.parentElement.parentElement.id;
+		browser.storage.local.get('filters').then(function(item){return removeFilter(filterType, filterToRemove, item)}, onError);
+		this.parentElement.outerHTML = '';
+	}
+
+	li.appendChild(removeBtn);
+	console.log('lalala');
+	return li;
+}
+
+function removeFilter(filterType, filterToRemove, item) {
+	delete item.filters[filterType][filterToRemove];
+	browser.storage.local.set({filters: item.filters});
+}
+
 function setFilter(filterType, toFilter, filterTo, item) {
 	item.filters[filterType] = item.filters.hasOwnProperty(filterType) ? item.filters[filterType] : {};
 	item.filters[filterType][toFilter] = filterTo;
 	browser.storage.local.set({filters: item.filters});
-	let ul = document.getElementById(filterType + 'List');
-	let li = document.createElement('li');
-	let liText = document.createTextNode(toFilter + " - " + filterTo);
-	li.appendChild(liText);
+	let ul = document.getElementById(filterType);
+	let li = createLi(toFilter, filterTo);
 	ul.appendChild(li);
 }
 
 function listFilters(item) {
 	const uls = ['users', 'words'];
 	uls.forEach(function(list, index){
-		let ul = document.getElementById(list + 'List');
+		let ul = document.getElementById(list);
 		for (var property in item.filters[list]) {
-			let li = document.createElement('li');
-			let liText = document.createTextNode(property + " - " + item.filters[list][property]);
-			li.appendChild(liText);
+			let li = createLi(property, item.filters[list][property]);
 			ul.appendChild(li);
 		}
 	});
@@ -40,6 +68,17 @@ document.getElementById('addBtn').onclick = function() {
 		document.getElementById('toFilter').value = '';
 		document.getElementById('filterTo').value = '';
 	}
-}
+};
 
-let filters = browser.storage.local.get("filters").then(listFilters, onError);
+document.getElementById('clearBtn').onclick = function() {
+	browser.storage.local.clear();
+	browser.storage.local.set({filters: {words: {}, users: {}}});
+	const uls = ['users', 'words'];
+	uls.forEach(function(list, index){
+		let ul = document.getElementById(list);
+		while (ul.firstChild)
+			ul.removeChild(ul.firstChild);
+	});
+};
+
+browser.storage.local.get("filters").then(listFilters, onError);
--- twilter.js
+++ twilter.js
@@ -1,32 +1,13 @@
-function onGot(item) {
-	console.log('a');
+let filters = browser.storage.local.get('filters');
+
+window.onload = function() {
+	let tweets = document.getElementsByClassName('tweet');
+	for (i=0, i<tweets.length; i++) {
+		let tweet = tweets[i];
+		let username = tweet.getAttribute('data-screen-name');
+		// content é sempre segundo elemento de tweet
+		// iterar sobre as children do content
+		// texto vai estar na children do content com class js-tweet-text-container
+		// apagar imagem (se tiver) - children do content com class AdaptativeMediaOuterContainer
+	}
 }
-
-function onGota(filterType, user, filterTo, item) {
-	console.log(item);
-	item.filters[filterType] = item.filters.hasOwnProperty(filterType) ? item.filters[filterType] : {};
-	console.log(filterType);
-	console.log(item.filters[filterType]);
-	item.filters[filterType][user] = filterTo;
-	console.log(item.filters);
-	browser.storage.local.set({filters: item.filters});
-}
-
-
-function onError(error) {
-	console.log(error)
-}
-
-browser.storage.local.get().then(function(item) {
-	if (!item.hasOwnProperty('filters'))
-		browser.storage.local.set({filters: {}});
-	}, onError);
-
-let user = "teste";
-let filterTo = "pffff";
-if (user != "" && filterTo != "") {
-	let filterType = "words";
-	let filtersGot = browser.storage.local.get("filters").then(function(item) {return onGota(filterType, user, filterTo, item)}, onError);
-}
-
-//let filters = browser.storage.local.get("filters").then(onGot, onError);
